21e6c5423aa5a0ffa2e2dc9f9ff5c52d932e8a05
CarlosWindows <cadudailha68@gmail.com>
2026-02-25 13:13:59 -0300

    feat: Create struct semantical the amazon and checkout HTML
    - Create header and main tags at amazon and checkout
    - Remove the default value at cart

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,5 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
-if (!cart) {
-  cart = [{
-    id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 1
-  }]
-}
-
-console.log(cart);
-
 function saveToLocalStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
 }
